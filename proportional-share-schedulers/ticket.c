e9f8dcb024679b3f71995b1a72de84c6ce54ea77
Anish Mani <anish1mani@gmail.com>
2025-08-11 09:40:24 -0400

    implemented getopt_long options for psuedorandom mode and a help option

--- proportional-share-schedulers/ticket.c
+++ proportional-share-schedulers/ticket.c
@@ -1,13 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
 
+int psuedo_random();
 
+/* Main function will take care of console input and processing using *getopt*
+ * and calls functions in order to execute compartmentalizable tasks
+ */
 
-int main() {
+int main(int argc, char** argv) {
 
-    while (1){
+    int c;
+
+    static const struct option long_options[] = {
+        {"help",          no_argument, NULL, 'h'},
+        {"psuedo-random", no_argument, NULL, 'p'}
+
+    };
+
+    c = getopt_long(argc, argv, "p", long_options, NULL);
+
+    switch (c){
+        case 'h':
+            printf("help\n");
+            break;
+        case 'p':
+            printf("p?\n");
+            break;
+        default:
+            printf("default case\n");
 
     }
+
+
+    //getopt_long();
+
     
 
 
