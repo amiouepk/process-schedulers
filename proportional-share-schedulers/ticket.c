e5a56d960772058f1a2f443ae6e931849cb69259
Anish Mani <anish1mani@gmail.com>
2025-08-13 07:23:38 -0400

    Finished all functions, however the randomness of the the psudeo random method leaves much to be desired. For example, it is generating smaller numbers much more often than larger numbers, so the assignment of the range of the tickets makes a large difference; the placemet of the ranges in the total number of tickets impacts how often the process runs more than the number of tickets a process ahs.

--- proportional-share-schedulers/ticket.c
+++ proportional-share-schedulers/ticket.c
@@ -18,10 +18,16 @@ struct process {
     int* range;
 };
 
+struct intarray {
+    int* array;
+    int size;
+};
+
 int psuedo_random();
 //void range_assign(struct process* proc_arr, int num_proc, int ticket_num);
-void ticket_draw(struct process* proc_arr, int num_proc, int ticket_num);
+void ticket_draw(struct process* proc_arr, int num_proc, struct intarray* run_order, int rand_num, int order_index);
 struct process* process_create(int num_proc, int* proc_tickets);
+void process_print(struct process* proc_arr, int num_proc, struct intarray* run_order, int* rn_arr);
 
 /* Main function will take care of console input and processing using *getopt*
  * and calls functions in order to execute compartmentalizable tasks
@@ -62,8 +68,8 @@ int psuedo_random() {
     int sim_length = 100; // needs to be variable
     int num_proc = 2; // needs to be variable
     int* proc_tickets = malloc(num_proc * sizeof(int)); // needs to be variable
-    proc_tickets[0] = 75; // needs to be variable
-    proc_tickets[1] = 25;// needs to be variable
+    proc_tickets[0] = 25; // needs to be variable
+    proc_tickets[1] = 75;// needs to be variable
 
     struct timespec tp;
     int cgt_err;
@@ -72,7 +78,11 @@ int psuedo_random() {
     int draw_num = 0; // number of times the tickets are drawn
 
     struct process* proc_arr = process_create(num_proc, proc_tickets);
-    //int** ranges = malloc(num_proc * sizeof(int*));
+    struct intarray run_order;
+    run_order.array = malloc(ticket_num * sizeof(int));
+    run_order.size = ticket_num;
+    int* rn_arr = malloc(run_order.size * sizeof(int));
+
 
     //range_assign(proc_arr, num_proc, ticket_num);
 
@@ -85,40 +95,32 @@ int psuedo_random() {
         }
 
         unsigned int seed = (tp.tv_sec >> 2) + (tp.tv_nsec >> 2);
-        srand(seed);
+        srandom(seed);
         //printf("seed: %i\n", seed);
  
-        rand_num = rand();
+        rand_num = random();
         do {
             rand_num = (rand_num / 10) + 1;
         } while (rand_num > ticket_num);
 
+        rn_arr[draw_num] = rand_num;
+
+        ticket_draw(proc_arr, num_proc, &run_order, rand_num, draw_num);
+
         draw_num++;
     }
 
+    process_print(proc_arr, num_proc, &run_order, rn_arr);
 
-    free(proc_tickets);
+    for (int i = 0; i < num_proc; i++){
+        free(proc_arr[i].range);
+    }
     free(proc_arr);
+    free(proc_tickets);
+    free(run_order.array);
 
     return 0;
 }
-
-// void range_assign(struct process* proc_arr, int num_proc, int ticket_num) {
-    
-//     int prev_alloc = 0;
-
-//     for (int i = 0; i < num_proc; i++){
-//         ranges[i] = malloc(2 * sizeof(int));
-
-//         ranges[i][0] = prev_alloc + 1;
-//         ranges[i][1] = prev_alloc + proc_arr[i].alloc_tickets;
-
-//         printf("low: %d, high: %d\n", ranges[i][0], ranges[i][1]);  
-
-//         prev_alloc = proc_arr[i].alloc_tickets;
-//     }
-
-// }
 
 
 struct process* process_create(int num_proc, int* proc_tickets){
@@ -132,10 +134,10 @@ struct process* process_create(int num_proc, int* proc_tickets){
         proc_arr[i].alloc_tickets = proc_tickets[i];
         proc_arr[i].range = malloc(2 * sizeof(int));
 
-        proc_arr->range[0] = prev_alloc + 1;
-        proc_arr->range[1] = prev_alloc + proc_arr[i].alloc_tickets;
+        proc_arr[i].range[0] = prev_alloc + 1;
+        proc_arr[i].range[1] = prev_alloc + proc_arr[i].alloc_tickets;
 
-        printf("low: %d, high: %d\n", proc_arr->range[0], proc_arr->range[1]);  
+        printf("low: %d, high: %d\n", proc_arr[i].range[0], proc_arr[i].range[1]);  
 
         prev_alloc = proc_arr[i].alloc_tickets;
 
@@ -145,15 +147,54 @@ struct process* process_create(int num_proc, int* proc_tickets){
 
 }
 
-void ticket_draw(struct process* proc_arr, int num_proc, int ticket_num){
+void ticket_draw(struct process* proc_arr, int num_proc, struct intarray* run_order, int rand_num, int order_index){
 
-
-
-
-
+    for (int i = 0; i < num_proc; i++){
+        if (rand_num >= proc_arr->range[0] && rand_num <= proc_arr->range[1]){
+            run_order->array[order_index] = proc_arr[i].proc_num;
+        }
+    }
 }
 
+void process_print(struct process* proc_arr, int num_proc, struct intarray* run_order, int* rn_arr){
+    printf("Result: \n");
 
+    for (int i = 0; i < run_order->size; i++) {
+        if (i < 10) {
+            printf("%i   ", i);
+            continue;
+        }
+        
+        printf("%i  ", i);
+    }
+
+    printf("\n");
+    printf("\n");
+
+    for (int i = 0; i < run_order->size; i++){
+        if (rn_arr[i] < 10){
+            printf("%i   ", run_order->array[i]);
+            continue;
+        }
+        printf("%i  ", rn_arr[i]);
+
+    }
+
+    printf("\n");
+    printf("\n");
+
+    for (int i = 0; i < run_order->size; i++){
+        if (run_order->array[i] < 10){
+            printf("%i   ", run_order->array[i]);
+            continue;
+        }
+        printf("%i  ", run_order->array[i]);
+
+    }
+
+    printf("\n");
+
+}
 
 
 
